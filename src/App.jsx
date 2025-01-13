18f7bfc0ddf39e25eb045e5521446ae3069741ee
festus-sulumeti <sulumetifestus@outlook.com>
2025-01-13 14:18:39 +0300

    update: App file made well

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import VideoInput from './components/VideoInput';
-import SummaryDisplay from './components/SummaryDisplay';
 import LandingPage from './components/LandingPage';
 import ClipInsight from './components/ClipInsight';
 
 const App = () => {
-    const [summary, setSummary] = useState('');
-
-    const fetchSummary = async (videoId) => {
-        try {
-            const videoResponse = await fetch(`/video/${videoId}`);
-            const videoDetails = await videoResponse.json();
-            const text = videoDetails.description;
-
-            const summaryResponse = await fetch('/summarize', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ text })
-            });
-            const summaryData = await summaryResponse.json();
-            setSummary(summaryData.summary);
-        } catch (error) {
-            console.error('Error fetching summary:', error);
-            alert('Error fetching summary');
-        }
-    };
-
     return (
         <Router>
             <Routes>
